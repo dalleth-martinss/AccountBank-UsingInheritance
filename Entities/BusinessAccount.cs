37a2eef8e10f253465dcc4f831066533ad71ac9f
dalleth-martinss <essi_dalleth@outlook.com>
2024-06-29 10:34:14 -0300

    Creating exercise Bank account with inheritance

---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+namespace ExerciseAccountBankUsandoHeranca.Entities
+{
+     class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+   
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance)
+        {
+            double LoanLimit = loanLimit;
+        }
+
+
+
+    }
+}
+
